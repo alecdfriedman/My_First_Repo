3d8fbef15a4005632906c7d7d67836d121e88b16
alecdfriedman <83744445+alecdfriedman@users.noreply.github.com>
2022-08-31 22:59:51 -0500

    Update index.js
    Got rid of the annoying pop-up

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-$(document).ready(function(){
-    alert("Hello! Thanks for visiting my website! It's a work in progress, so pardon the mess.\n\nFeel free to take a look around!")
- });
+//$(document).ready(function(){
+//    alert("Hello! Thanks for visiting my website! It's a work in progress, so pardon the mess.\n\nFeel free to take a look around!")
+// });
